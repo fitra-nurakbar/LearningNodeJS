92cc677b29e5c8262153e6e034e59ce446c09e62
nando8 <fitranurakbar378@gmail.com>
2022-08-04 10:03:38 +0700

    core module write file asynchronous

--- app.js
+++ app.js
@@ -4,8 +4,18 @@
 const fs = require('fs');
 
 // menuliskan string ke file (synchronous)
-try {
-     fs.writeFileSync('test/test.txt', 'Hello world syncrhonous');
-} catch (e){
+// try {
+//      fs.writeFileSync('test/test.txt', 'Hello World Syncrhonous');
+// } catch (e){
+//      console.log(e)
+// }
+
+// menuliskan string ke file (asynchronous)
+fs.writeFile('test/test.txt', 'Hello World Asynchronous', (e) => {
      console.log(e)
-}
+});
+
+// membaca isi file (synchronous)
+// const data = fs.readFileSync('test/test.txt', 'utf-8');
+// // console.log(data.toString())
+// console.log(data)
