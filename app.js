2cbaf215cef26dfca89ae20cac3d049a62ca8a32
nando8 <fitranurakbar378@gmail.com>
2022-08-04 10:20:55 +0700

    membaca file (asynchronous)

--- app.js
+++ app.js
@@ -11,12 +11,17 @@ const fs = require('fs');
 // }
 
 // menuliskan string ke file (asynchronous)
-fs.writeFile('test/test.txt', 'Hello World Asynchronous', (e) => {
-     console.log(e)
-});
+// fs.writeFile('test/test.txt', 'Hello World Asynchronous', (e) => {
+//      console.log(e)
+// });
 
 // membaca isi file (synchronous)
-const data = fs.readFileSync('test/test.txt', 'utf-8');
-// console.log(data.toString())
-console.log(data)
+// const data = fs.readFileSync('test/test.txt', 'utf-8');
+// // console.log(data.toString())
+// console.log(data)
 
+// membaca file (asynchronous)
+fs.readFile('tes/test.txt', 'utf-8', (e, data) => {
+     if (e) throw e;
+     console.log(data);
+});
