c170ea09cf7e73d9c1c2eae7a02606a2b319a59b
nando8 <fitranurakbar378@gmail.com>
2022-08-11 13:22:17 +0700

    memisahkan file entry point

--- app.js
+++ app.js
@@ -2,31 +2,6 @@
 // File System
 
 const fs = require("fs");
-const { resolve } = require("path");
-const { rejects } = require("assert");
-
-// menuliskan string ke file (synchronous)
-// try {
-//      fs.writeFileSync('test/test.txt', 'Hello World Syncrhonous');
-// } catch (e){
-//      console.log(e)
-// }
-
-// menuliskan string ke file (asynchronous)
-// fs.writeFile('test/test.txt', 'Hello World Asynchronous', (e) => {
-//      console.log(e)
-// });
-
-// membaca isi file (synchronous)
-// const data = fs.readFileSync('test/test.txt', 'utf-8');
-// // console.log(data.toString())
-// console.log(data)
-
-// membaca file (asynchronous)
-// fs.readFile('tes/test.txt', 'utf-8', (e, data) => {
-//      if (e) throw e;
-//      console.log(data);
-// });
 
 // Readline
 const readline = require("readline");
@@ -48,27 +23,22 @@ if (!fs.existsSync(dataPath)) {
 
 const mauTanya = (pertanyaan) => {
   return new Promise((resolve, rejects) => {
-    rl.question(pertanyaan,
-      (inputan) => {
-        resolve(inputan);
-      });
+    rl.question(pertanyaan, (inputan) => {
+      resolve(inputan);
+    });
   });
 };
 
-const main = async () => {
-  const nama = await mauTanya('Masukan Nama Anda : ')
-  const noHP = await mauTanya('Masukan No HP Anda : ')
-  const email = await mauTanya('Masukan Email Anda : ')
-
-  const data = { nama, noHP, email};
+const simpanContact = ( nama, noHP, email ) => {
+  const data = { nama, noHP, email };
   const file = fs.readFileSync("./data/contacts.json", "utf-8");
   const bio = JSON.parse(file);
 
   bio.push(data);
 
   fs.writeFileSync("./data/contacts.json", JSON.stringify(bio));
-  console.log(`Hallo ${nama}, Datanya udah masuk...`);
+  console.log(`Selamat ${nama}, Datanya udah masuk...`);
   rl.close();
 };
 
-main();
+module.exports = { mauTanya, simpanContact };
--- index.js
+++ index.js
@@ -1,35 +1,11 @@
-// const getUserSync = (id) => {
-//      const nama = id === 1 ? "Test Satu" : "Test Dua";
-//      return {id, nama};
-// }
+const { mauTanya, simpanContact } = require("./app.js");
 
-// const userSatu = getUserSync(1);
-// console.log(userSatu);
+const main = async () => {
+  const nama = await mauTanya("Masukan Nama Anda : ");
+  const noHP = await mauTanya("Masukan No HP Anda : ");
+  const email = await mauTanya("Masukan Email Anda : ");
 
-// const userDua = getUserSync(2);
-// console.log(userDua);
+  simpanContact(nama, noHP, email);
+};
 
-// const userTiga = "Test Tiga";
-// console.log(userTiga);
-
-const data = require("./test/coba");
-
-console.log(data.data("ahmad", 19), data.test, data.mahasiswa.ctkMhs(), new data.Orang);
-// const getUserAsync = (id, cb) => {
-//   const time = id === 1 ? 300 : 500;
-//   setTimeout(() => {
-//     const nama = id === 1 ? "Test Satu" : "Test Dua";
-//     cb({ id, nama });
-//   }, time);
-// };
-
-// const userSatu = getUserAsync(1, (hasil) => {
-//   console.log(hasil);
-// });
-
-// const userDua = getUserAsync(2, (hasil) => {
-//   console.log(hasil);
-// });
-
-// const userTiga = "Test Tiga";
-// console.log(userTiga);
+main();
