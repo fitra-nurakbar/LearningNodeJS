c87635bff6cc7b3a6ab302998433098eee0de923
nando8 <fitranurakbar378@gmail.com>
2022-08-11 10:19:19 +0700

    install package chalk

--- npm/app.js
+++ npm/app.js
@@ -1,1 +1,10 @@
-console.log("Hello test")
+// const validator = require('validator');
+const chalk = require('chalk');
+// console.log(validator)
+// const test = validator.isMobilePhone('089630958735', 'id-ID');
+// console.log(test);
+
+const error = chalk.bold.red('ERROR!!!');
+const test = chalk`{bold.blue Bold Blue} {bgYellow.bold.upper Background Blue}`;
+console.log(error);
+console.log(test);
