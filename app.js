83aac200c145446049813d684061fcc2fb62ab10
nando8 <fitranurakbar378@gmail.com>
2022-08-12 15:54:51 +0700

    App contact(menambah & validasi)

--- app.js
+++ app.js
@@ -2,13 +2,9 @@
 // File System
 
 const fs = require("fs");
-
-// Readline
-const readline = require("readline");
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
+const chalk = require("chalk");
+const validator = require("validator");
+const { argv } = require("process");
 
 // membuat directory jika belum ada
 const dirPath = "./data/";
@@ -21,24 +17,34 @@ if (!fs.existsSync(dataPath)) {
   fs.writeFileSync(dataPath, "[]", "utf-8");
 }
 
-const mauTanya = (pertanyaan) => {
-  return new Promise((resolve, rejects) => {
-    rl.question(pertanyaan, (inputan) => {
-      resolve(inputan);
-    });
-  });
+const simpanContact = (nama, noHP, email, argv) => {
+  const contact = { nama, noHP, email };
+  const fileBuffer = fs.readFileSync("./data/contacts.json", "utf-8");
+  const contacts = JSON.parse(fileBuffer);
+
+  // cek duplikat
+  const duplikat = contacts.find((contacts) => contacts.nama === nama);
+  if (duplikat) {
+    console.log(
+      chalk.red.bold("Nama contact sudah terdaftar, gunakan nama lain")
+    );
+    return false;
+  }
+  // cek email formatter
+  if (email) {
+    if (!validator.isEmail(email)) {
+      console.log(chalk.red.bold(`Format email tidak benar, contoh: '${email}@mail.com'`));
+      return false;
+    }
+  }
+if(!validator.isMobilePhone(noHP, 'id-ID')){
+  console.log(chalk.red.bold(`Format No Handphone tidak benar, contoh: '(+62)${noHP}'`))
+  return false;
+};
+  contacts.push(contact);
+
+  fs.writeFileSync("./data/contacts.json", JSON.stringify(contacts));
+  console.log(chalk.green.bold(`Selamat ${nama }, Datanya udah masuk...`));
 };
 
-const simpanContact = ( nama, noHP, email ) => {
-  const data = { nama, noHP, email };
-  const file = fs.readFileSync("./data/contacts.json", "utf-8");
-  const bio = JSON.parse(file);
-
-  bio.push(data);
-
-  fs.writeFileSync("./data/contacts.json", JSON.stringify(bio));
-  console.log(`Selamat ${nama}, Datanya udah masuk...`);
-  rl.close();
-};
-
-module.exports = { mauTanya, simpanContact };
+module.exports = { simpanContact };
--- index.js
+++ index.js
@@ -1,11 +1,47 @@
-const { mauTanya, simpanContact } = require("./app.js");
+const { command, parse } = require("yargs");
+const { simpanContact } = require("./app");
 
-const main = async () => {
-  const nama = await mauTanya("Masukan Nama Anda : ");
-  const noHP = await mauTanya("Masukan No HP Anda : ");
-  const email = await mauTanya("Masukan Email Anda : ");
+// mengambil argument dari command line
+// console.log(process.argv)
 
-  simpanContact(nama, noHP, email);
-};
+command({
+  command: "add",
+  describe: "Menambahkan contact baru",
+  builder: {
+    nama: {
+      describe: "Nama Lengkap",
+      demandOption: true,
+      type: "string",
+    },
+    noHP: {
+      describe: "No handphone",
+      demandOption: true,
+      type: "string",
+    },
+    email: {
+      describe: "Alamat Email",
+      demandOption: false,
+      type: "string",
+    },
+  },
+  handler(argv) {
+    const nama = argv.nama;
+    const email = argv.email;
+    const noHP = argv.noHP;
+    simpanContact(nama, noHP, email);
+  },
+});
 
-main();
+parse();
+
+// const { mauTanya, simpanContact } = require("./app.js");
+
+// const main = async () => {
+//   const nama = await mauTanya("Masukan Nama Anda : ");
+//   const noHP = await mauTanya("Masukan No HP Anda : ");
+//   const email = await mauTanya("Masukan Email Anda : ");
+
+//   simpanContact(nama, noHP, email);
+// };
+
+// main();
